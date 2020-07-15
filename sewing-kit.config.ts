71c29afc15d0ad2fa3104afd9efad11777e4cdc8
Brian Ryu <brian.ryu@shopify.com>
2020-07-15 19:38:44 -0400

    Get rid of repo test config

--- sewing-kit.config.ts
+++ sewing-kit.config.ts
@@ -1,8 +1,30 @@
 import {createWorkspace} from '@sewing-kit/config';
+import {createWorkspaceTestPlugin} from '@sewing-kit/plugins';
 import {eslint} from '@sewing-kit/plugin-eslint';
 import {jest} from '@sewing-kit/plugin-jest';
 import {workspaceTypeScript} from '@sewing-kit/plugin-typescript';
 
 export default createWorkspace(workspace => {
-  workspace.use(eslint(), jest(), workspaceTypeScript());
+  workspace.use(
+    eslint(),
+    jest(),
+    workspaceTypeScript(),
+    createWorkspaceTestPlugin('Quilt.NonPackageTests', ({hooks}) => {
+      hooks.configure.hook(hooks => {
+        hooks.jestConfig?.hook(config => ({
+          ...config,
+          projects: [
+            ...config.projects,
+            {
+              ...(config.projects.length ? config.projects[0] : {}),
+              displayName: 'quilt',
+              rootDir: '/Users/brianryu/src/github.com/Shopify/quilt/tests',
+              setupFiles: ['./setup.ts'],
+              setupFilesAfterEnv: ['./each-test.ts'],
+            },
+          ],
+        }));
+      });
+    }),
+  );
 });
