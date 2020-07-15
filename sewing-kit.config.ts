30a718be87c6d54ef83a558f62220af4db62d386
Brian Ryu <brian.ryu@shopify.com>
2020-07-15 18:06:53 -0400

    Clean up -ignore files
    Delete unused Jest config
    
    Get rid of unnecessary workspace build plugin

--- sewing-kit.config.ts
+++ sewing-kit.config.ts
@@ -1,24 +1,8 @@
 import {createWorkspace} from '@sewing-kit/config';
-import {createWorkspaceBuildPlugin} from '@sewing-kit/plugins';
 import {eslint} from '@sewing-kit/plugin-eslint';
 import {jest} from '@sewing-kit/plugin-jest';
 import {workspaceTypeScript} from '@sewing-kit/plugin-typescript';
 
 export default createWorkspace(workspace => {
-  workspace.use(
-    eslint(),
-    jest(),
-    workspaceTypeScript(),
-    createWorkspaceBuildPlugin('Quilt.Build', ({hooks}) => {
-      hooks.configure.hook(hooks => {
-        hooks.babelConfig?.hook(_ => ({
-          presets: [
-            ['babel-preset-shopify/node', {typescript: true}],
-            'babel-preset-shopify/react',
-          ],
-          sourceMaps: 'inline',
-        }));
-      });
-    }),
-  );
+  workspace.use(eslint(), jest(), workspaceTypeScript());
 });
