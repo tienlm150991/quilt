8430803e8c0cd3bf63173ac6186257be3e0ece04
Brian Ryu <brian.ryu@shopify.com>
2020-07-21 01:05:35 -0400

    Add support for legacy decorators

--- config/sewing-kit/index.ts
+++ config/sewing-kit/index.ts
@@ -9,6 +9,25 @@ import {javascript} from '@sewing-kit/plugin-javascript';
 import {typescript} from '@sewing-kit/plugin-typescript';
 import {buildFlexibleOutputs} from '@sewing-kit/plugin-package-flexible-outputs';
 
+function addLegacyDecoratorSupport(config) {
+  return {
+    presets: config.presets,
+    plugins: (config.plugins || []).map(plugin => {
+      const pluginName = Array.isArray(plugin) ? plugin[0] : plugin;
+
+      if (pluginName.includes('@babel/plugin-proposal-decorators')) {
+        return [pluginName, {legacy: true}];
+      } else if (
+        pluginName.includes('@babel/plugin-proposal-class-properties')
+      ) {
+        return [pluginName, {loose: true}];
+      } else {
+        return plugin;
+      }
+    }),
+  };
+}
+
 export function quiltPackage({binaryOnly = true, jestEnv = 'jsdom'} = {}) {
   return createComposedProjectPlugin<Package>('Quilt.Package', [
     javascript(),
@@ -21,11 +40,13 @@ export function quiltPackage({binaryOnly = true, jestEnv = 'jsdom'} = {}) {
     createProjectBuildPlugin('Quilt.PackageBuild', ({hooks}) => {
       hooks.target.hook(({hooks}) => {
         hooks.configure.hook(hooks => {
-          hooks.babelIgnorePatterns.hook(ext => [
+          hooks.babelIgnorePatterns?.hook(ext => [
             ...ext,
             '**/*.test.ts',
             '**/*.test.tsx',
           ]);
+
+          hooks.babelConfig?.hook(addLegacyDecoratorSupport);
         });
       });
     }),
@@ -45,13 +66,7 @@ export function quiltPackage({binaryOnly = true, jestEnv = 'jsdom'} = {}) {
           ],
         }));
 
-        hooks.babelConfig?.hook(() => ({
-          presets: [
-            ['babel-preset-shopify/node', {typescript: true}],
-            'babel-preset-shopify/react',
-          ],
-          sourceMaps: 'inline',
-        }));
+        hooks.babelConfig?.hook(addLegacyDecoratorSupport);
       });
     }),
   ]);
